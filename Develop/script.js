263995c7151f7f8754b360794ce15f66df7b1136
ala decoste <alshareefialaa@gmail.com>
2020-12-29 23:58:50 -0500

    set save button on click.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,16 @@
 //Get current date by using moment.js
 $("#currentDay").text(moment().format('dddd, MMMM Do'))
 
+
+$(document).ready(function () {
+    // save button
+    $(".saveBtn").on("click", function () {
+        //Source:https://stackoverflow.com/questions/
+        //7463242/how-do-i-select-a-sibling-element-using-jquery
+        let timeID = $(this).parent().attr("id");
+        let input = $(this).siblings(".description").val();
+       
+        // Save text in local storage
+        localStorage.setItem(timeID, input);
+    })
+    
